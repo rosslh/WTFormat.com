63be1a22f1cda573fa2c333017673d4e2ba79c35
rosslh <ross@rosshill.ca>
2018-07-17 15:55:06 -0400

    better date generation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
 let time;
 
-function numDuplicateAtoms(timestamp) {
-  const parts = dateAtoms.map(x => timestamp.format(x));
-  return parts.length - new Set(parts).size;
-}
-
-function generateDates() {
-  const rightNow = moment().unix();
-  const good = [];
-  let tries = 0;
-  while (tries < 100000) {
-    const newTS = moment.unix(Math.floor(Math.random() * rightNow));
-    if (numDuplicateAtoms(newTS) <= 1) {
-      good.push(newTS.format('X'));
-    }
-    tries += 1;
-  }
-  console.log(good); // eslint-disable-line no-console
-}
-
-function main() {
-  time = moment(dates[Math.floor(Math.random() * dates.length)], 'X');
-  document.getElementById('input').placeholder = time.format('YYYY-MM-DD');
-  document.getElementById('shownDate').innerText = time.format('dddd, MMMM D, YYYY h:mm:ss A');
-}
-
 function filterResults(date) {
   return date;
 }
@@ -41,18 +16,21 @@ function joinSuffixes(arr) {
   return output;
 }
 
-function evaluateDate() {
-  let currentDate = document.getElementById('input').value.match(/(\d+|[A-Za-z]+|[^\dA-Za-z]+)/g) || [];
-  currentDate = joinSuffixes(currentDate);
+function group(str) {
+  return str.match(/(\d+|[A-Za-z]+|[^\dA-Za-z]+)/g);
+}
+
+function getFormats(timestamp, currentDate = group(document.getElementById('input').value)) {
+  const date = joinSuffixes(currentDate);
   let possibilities = [''];
-  for (const part of currentDate) {
+  for (const part of date) {
     let newPos = [];
     if (!part.match(/^[a-zA-Z0-9]+$/)) {
       newPos = newPos.concat(possibilities.map(x => x + part));
     } else {
       let matchFound = false;
       for (const atom of dateAtoms) {
-        if (part === time.format(atom)) {
+        if (part === timestamp.format(atom)) {
           newPos = newPos.concat(possibilities.map(x => x + atom));
           matchFound = true;
         }
@@ -63,21 +41,46 @@ function evaluateDate() {
     }
     possibilities = newPos;
   }
-  possibilities = possibilities.filter(filterResults);
+  return possibilities.filter(filterResults);
+}
+
+function generateDates() {
+  const rightNow = moment().unix();
+  const good = [];
+  let tries = 0;
+  while (tries < 10000) {
+    const newTS = moment.unix(Math.floor(Math.random() * rightNow));
+    const formats = getFormats(moment(newTS, 'X'), group(newTS.format('dddd, MMMM D, YYYY h:mm:ss A')));
+    if (formats.length === 1) {
+      good.push(newTS.format('X'));
+    }
+    tries += 1;
+  }
+  console.log(good); // eslint-disable-line no-console
+}
+
+function main() {
+  time = moment(dates[Math.floor(Math.random() * dates.length)], 'X');
+  document.getElementById('input').placeholder = time.format('YYYY-MM-DD');
+  document.getElementById('shownDate').innerText = time.format('dddd, MMMM D, YYYY h:mm:ss A');
+}
+
+function evaluateDate() {
+  const formats = getFormats(time);
   const list = document.getElementById('output');
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
-  if (!possibilities.length) {
+  if (!formats.length) {
     const entry = document.createElement('li');
     const text = document.createTextNode('Enter a date and click generate to see your date format!');
     entry.appendChild(text);
     list.appendChild(entry);
   }
-  for (const pos of possibilities) {
+  for (const f of formats) {
     const entry = document.createElement('li');
     const code = document.createElement('code');
-    const text = document.createTextNode(pos);
+    const text = document.createTextNode(f);
     code.appendChild(text);
     entry.appendChild(code);
     list.appendChild(entry);
